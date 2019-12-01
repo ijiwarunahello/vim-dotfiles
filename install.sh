c317719ad6cd6160ccff8540d371a2980e9a653e
Kazuya Iriguchi <baskin.robbins1945@gmail.com>
2019-12-01 22:36:07 +0900

    add: comment

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ CURRENT_DIR=`pwd`
 
 # install vimplug if not exists
 if [ -e ~/.vim/autoload/plug.vim ]; then
-    echo "vimplug already installed."
+    echo "vimplug already installed. skipped."
 else
+    echo "vimplug install..."
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo "done."
 fi
 
 # setting symbolic link
