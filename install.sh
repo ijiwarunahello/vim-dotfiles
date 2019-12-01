33fd8505329fdf083cbec1b606e68d8ef1e45963
Kazuya Iriguchi <baskin.robbins1945@gmail.com>
2019-12-01 21:52:59 +0900

    add: install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+# constant
+CURRENT_DIR=`pwd`
+VIMPLUG_INSTALL_DIR="~/.vim/autoload/"
+VIMPLUG_FILENAME="plug.vim"
+
+# install vimplug if not exists
+if [ -e ~/.vim/autoload/plug.vim ]; then
+    echo "vimplug already installed."
+else
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# setting symbolic link
+echo "setting .vimrc symbolic..."
+ln -sf $CURRENT_DIR/.vimrc ~/.vimrc
