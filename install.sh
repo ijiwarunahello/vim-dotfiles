4d221adbf0d73f09594c9a8fc005edf75aac3510
ijiwarunahello <baskin.robbins1945@gmail.com>
2019-12-04 13:03:08 +0900

    add: color to output message

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ CURRENT_DIR=`pwd`
 
 # install vimplug if not exists
 if [ -e ~/.vim/autoload/plug.vim ]; then
-    echo "vimplug already installed. skipped."
+    echo -e "\e[1;32mvimplug already installed. skipped.\e[m"
 else
-    echo "vimplug install..."
+    echo -e "\e[1;33mvimplug install...\e[m"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    echo "done."
+    echo -e "\e[1;32mdone.\e[m"
 fi
 
 echo -e "\e[1;33mcreate vimrc...\e[m"
